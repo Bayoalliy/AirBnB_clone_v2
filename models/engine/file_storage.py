3604a9d5c1810fd79fb2b1b047a1d2fb2fb1f57b
adebayo <adebayoalliy@gmail.com>
2024-07-18 04:32:37 -0700

    delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            temp = {}
+            for key, value in FileStorage.__objects.items():
+                temp[key] = value
+            return temp
+
         return FileStorage.__objects
 
     def new(self, obj):
@@ -24,6 +30,12 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        if obj:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del(self.__objects[key])
 
     def reload(self):
         """Loads storage dictionary from file"""
