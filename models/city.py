f5b0305f5919d140cecf13058d2871516a3e6ae1
Bayo <adebayoalliy@gmail.com>
2025-05-04 07:48:25 -0700

    gg

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
@@ -9,3 +9,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    state = relationship('State', back_populates='cities')
--- models/state.py
+++ models/state.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
-from models.city import City
+"""
+Write a class State that inherits from BaseModel:
 
+models/state.py
+Public class attributes:
+name: string - empty string
+
+Update FileStorage to manage correctly serialization and
+deserialization of State.
+
+Update your command interpreter (console.py) to allow show,
+create, destroy, update and all used with State.
+"""
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, Integer, String, ForeignKey
+from os import getenv
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
+    """inherits from BaseModel and defines the state object"""
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', back_populates='state', cascade="all, delete-orphan")
+    else:
+        from models import storage
+        name = ""
 
-    @property
-    def cities(self):
-        """getter of cities attribut"""
-        tmp = []
-        for value in sorage.all().values():
-            if value.__class__ == City:
-                if value.state_id == self.id:
-                    tmp.push(value)
-        return tmp
+        @property
+        def cities(self):
+            cities_lst = []
+            for k, v in storage.all().items():
+                if type(v) == City and v.state_id == self.id:
+                    cities_lst.push(v)
+            return cities_lst
