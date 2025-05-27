60667d629fa8cbd6e109b5b3521642f4f212c419
adebayo <adebayoalliy@gmail.com>
2025-05-27 04:54:02 -0700

    ff

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 echo "<html><body><h1>Heloo worlddd!</h1></body></html>" > /data/web_static/releases/test/index.html
 mkdir -p /data/web_static/shared/
-ln -sfn /data/web_static/releases/test/ /data/web_static/releases/test/
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sed '/location/i \\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
