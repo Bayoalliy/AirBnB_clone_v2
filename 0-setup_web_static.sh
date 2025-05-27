9c50a11d86f6e22f20eae1ae045bd3f300fbc0ba
adebayo <adebayoalliy@gmail.com>
2025-05-27 01:51:25 -0700

    ff

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #Write a Bash script that sets up your web servers for the deployment of web_static.
+sudo apt-get install nginx
 mkdir -p /data/web_static/releases/test/
 echo "<html><body><h1>Heloo worlddd!</h1></body></html>" > /data/web_static/releases/test/index.html
 mkdir -p /data/web_static/shared/
