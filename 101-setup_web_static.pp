ff982139f852cff7cf8a4b92428ed5bcda9a5885
adebayo <adebayoalliy@gmail.com>
2025-05-29 14:30:30 -0700

    rr

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -40,7 +40,7 @@ exec {'change ownership':
 }
 
 exec {'configure nginx':
-    command => "sed -i '/pass PHP/i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default",
+    command => 'sed -i \'/pass PHP/i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\' /etc/nginx/sites-available/default',
     path    => ['/usr/bin', '/usr/sbin'],
 }
 
