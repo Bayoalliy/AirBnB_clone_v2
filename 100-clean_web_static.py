181d4a41a7dc8892af4f438e583beea4009375f9
adebayo <adebayoalliy@gmail.com>
2025-05-29 09:51:31 -0700

    gg

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,47 +1,22 @@
 #!/usr/bin/python3
-"""
-Write a Fabric script (based on the file 3-deploy_web_static.py)
-that deletes out-of-date archives, using the function do_clean:
-"""
-
+""" Function that deploys """
 from fabric.api import *
-from datetime import datetime
-import os
 
 
-env.hosts = ['3.82.128.8', '3.83.155.83']
-env.user = 'ubuntu'
-
-
-def local_clean(n):
-    res = local('ls versions/', capture=True)
-    if res.succeeded:
-        lst = sorted(res.splitlines())
-        lst = lst[::-1]
-        if not int(n):
-            lst = lst[1:]
-        else:
-            lst = lst[int(n):]
-
-    for file in lst:
-        local('rm versions/{}'.format(file))
-
-
-def remote_clean(n):
-    res = run('ls /data/web_static/releases')
-    if res.succeeded:
-        lst = sorted(res.split(' '))
-        lst = [f for f in lst if f.startswith("web_static")]
-        lst = lst[::-1]
-        if not int(n):
-            lst = lst[1:]
-        else:
-            lst = lst[int(n):]
-    for file in lst:
-        sudo('rm -r /data/web_static/releases/{}'.format(file))
+env.hosts = ['35.231.33.237', '34.74.155.163']
+env.user = "ubuntu"
 
 
 def do_clean(number=0):
-    """removes outdated archives"""
-    local_clean(number)
-    remote_clean(number)
+    """ CLEANS """
+
+    number = int(number)
+
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
