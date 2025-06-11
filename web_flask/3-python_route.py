de22736859a0ae66e7562aa792eb1094c1410c09
adebayo <adebayoalliy@gmail.com>
2025-06-11 10:12:11 -0700

    cisfun

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -34,7 +34,7 @@ def c_is_fun(text):
 
 
 @app.route("/python/<text>", strict_slashes=False)
-def c_is_fun(text="is cool"):
+def py_is_fun(text="is cool"):
     text = text.replace('_', ' ')
     return "Python " + text
 
