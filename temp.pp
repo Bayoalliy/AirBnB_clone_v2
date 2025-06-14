d55db2224ec7d1db7583050827d219ba24a23a62
adebayo <adebayoalliy@gmail.com>
2025-06-14 09:10:10 -0700

    cpied

--- temp.pp
+++
@@ -1,4 +0,0 @@
-exec {'configure nginx':
-    command => 'sed -i \'/pass PHP/i\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\' /etc/nginx/sites-available/default',
-    path    => ['/usr/bin', '/usr/sbin'],
-}
